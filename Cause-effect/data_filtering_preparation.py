c32f3b1832d068bd6330a3772457fadb3326fac8
Diviyan Kalainathan <dkalainathan@gmail.com>
2016-09-07 15:11:20 +0200

    [C/E] Debug + Added functionalities Signed-off-by: Diviyan Kalainathan <dkalainathan@gmail.com>

--- Cause-effect/data_filtering_preparation.py
+++ Cause-effect/data_filtering_preparation.py
@@ -10,6 +10,7 @@ import os
 
 inputfolder = 'output/obj8/'
 var_info = 'input/Variables_info.csv'
+verbose=False
 
 # Loading data info
 print('--Loading data & parameters--')
@@ -20,18 +21,20 @@ with open('input/Variables_info.csv', 'rb') as datafile:
     type_var = []
     num_bool = []
     spec_note = []
-
+    ignored=[]
     for var_row in var_reader:  # Taking flags into account
-        name_var += [var_row[0], var_row[0] + '_flag']
-        type_var += [var_row[1], 'B']
-        num_bool += [var_row[3], str(2)]
-        spec_note += [var_row[4], str(-2)]
-print len(name_var)
+        if not(var_row[0][-4:]=='drap' or var_row[4]=='-1' or var_row[4]=='I'):
+            name_var += [var_row[0], var_row[0] + '_flag']
+            type_var += [var_row[1], 'B']
+            num_bool += [var_row[3], str(2)]
+            spec_note += [var_row[4], str(-2)]
+
+print(len(name_var))
 # Generate files for all clusters
 print('--Generating input files--')
 cluster_n = 0
 while os.path.exists(inputfolder + 'cluster_' + str(cluster_n)):
-    cluster_path = inputfolder + 'cluster_c_' + str(cluster_n) + '/data_c_' + str(cluster_n) + '.csv'
+    cluster_path = inputfolder + 'cluster_' + str(cluster_n) + '/data_c_' + str(cluster_n) + '.csv'
 
     print('--- Cluster n : ', cluster_n)
     # No filtering done
@@ -40,6 +43,8 @@ while os.path.exists(inputfolder + 'cluster_' + str(cluster_n)):
     with open(cluster_path, 'rb') as inputfile:
         datareader = csv.reader(inputfile, delimiter=';', quotechar='|')
         c_data_header = next(datareader)
+
+    print(len(c_data_header))
 
     # Create output files
     with open(inputfolder + 'cluster_' + str(cluster_n) + '/pairs_c_' + str(cluster_n) + '.csv',
@@ -57,14 +62,21 @@ while os.path.exists(inputfolder + 'cluster_' + str(cluster_n)):
         datawriter = csv.writer(sortedfile, delimiter=';', quotechar='|')
         datawriter.writerow(['SampleID', 'Target'])
 
+    #Load cluster data
+    cluster_data= numpy.loadtxt(cluster_path, skiprows=1,delimiter=';')
+    if verbose : print(cluster_data.shape)
+
     # Browse through vars
     for var_1 in range(len(c_data_header) - 1):
+        print(' ')
+        print('Var : ',c_data_header[var_1], '  -'),
         for var_2 in range(var_1 + 1, len(c_data_header)):
-
+            print('.'),
             if not (var_1 + 1 == var_2 and c_data_header[var_2][-4:] == 'flag'):  # Useless to consider
                 # the case of a var and its flag
-                data_to_load = [var_1, var_2]
 
+                data_to_load = [var_1, var_2]
+                if verbose: print(c_data_header[var_1],'-',c_data_header[var_2]),
                 # Taking flags into account when loading the data
                 if c_data_header[var_1][-4:] == 'flag':
                     var_1_flag = True
@@ -80,35 +92,38 @@ while os.path.exists(inputfolder + 'cluster_' + str(cluster_n)):
 
                 #Loading data
                 data_to_load.sort()
-                var_data = numpy.loadtxt(cluster_path, skiprows=1, usecols=data_to_load)
+                var_data=cluster_data[:,data_to_load]
 
-                if cluster_n == 0:
-                    print(var_data.shape)
 
                 SampleID = c_data_header[var_1] + '-' + c_data_header[var_2]
                 data_A = []
                 data_B = []
-
+                if verbose: print(data_to_load)
                 # Load only data if there is no missing values
+
                 for ppl in range(var_data.shape[0]):
-                    if (var_1_flag or var_data[ppl, 1] == '1') and (
-                                var_2_flag or var_data[ppl, var_data.shape[1] - 1] == '1'):
+                    # print(var_data[ppl, 1],var_data[ppl, var_data.shape[1] - 1])
+                    #print((var_1_flag or int(var_data[ppl, 1]) == 1) and (
+                    #            var_2_flag or int(var_data[ppl, var_data.shape[1] - 1]) == 1))
+                    if (var_1_flag or int(var_data[ppl, 1]) == 1) and (
+                                var_2_flag or int(var_data[ppl, var_data.shape[1] - 1]) == 1):
                         # if the data is valid
-                        data_A += var_data[ppl, 0]
+                        data_A.append(var_data[ppl, 0])
 
                         if var_1_flag:
-                            data_B += var_data[ppl, 1]
+                            data_B.append(var_data[ppl, 1])
                         else:
-                            data_B += var_data[ppl, 2]
+                            data_B.append(var_data[ppl, 2])
 
+                if verbose: print(len(data_A))
                 if len(data_A) > 25:
 
                     # Convert data_A and data_B into the right shape
                     A_values = ''
                     B_values = ''
                     for val in range(len(data_A)):
-                        A_values = A_values + ' ' + data_A[val]
-                        B_values = B_values + ' ' + data_B[val]
+                        A_values = A_values + ' ' + str(data_A[val])
+                        B_values = B_values + ' ' + str(data_B[val])
 
                     #Type of A & B
                     if type_var[var_1] == 'C':
@@ -133,24 +148,24 @@ while os.path.exists(inputfolder + 'cluster_' + str(cluster_n)):
 
                     # Updating pairs head
                     with open(inputfolder + 'cluster_' + str(cluster_n) + '/pairs_c_' + str(cluster_n) + '.csv',
-                              'a') as sortedfile:
-                        datawriter = csv.writer(sortedfile, delimiter=';', quotechar='|',
+                              'a') as outputfile:
+                        datawriter = csv.writer(outputfile, delimiter=';', quotechar='|',
                                                 lineterminator='\n')
                         datawriter.writerow([SampleID, A_values, B_values])
 
                     # Updating public info
                     with open(inputfolder + 'cluster_' + str(cluster_n) + '/publicinfo_c_' + str(
                             cluster_n) + '.csv',
-                              'a') as sortedfile:
-                        datawriter = csv.writer(sortedfile, delimiter=';', quotechar='|',
+                              'a') as outputfile:
+                        datawriter = csv.writer(outputfile, delimiter=';', quotechar='|',
                                                 lineterminator='\n')
                         datawriter.writerow([SampleID, typeA , typeB])
 
                     # Updating predictions head
                     with open(inputfolder + 'cluster_' + str(cluster_n) + '/predictions_c_' + str(
                             cluster_n) + '.csv',
-                              'a') as sortedfile:
-                        datawriter = csv.writer(sortedfile, delimiter=';', quotechar='|',
+                              'a') as outputfile:
+                        datawriter = csv.writer(outputfile, delimiter=';', quotechar='|',
                                                 lineterminator='\n')
                         datawriter.writerow([SampleID, '0'])
 
