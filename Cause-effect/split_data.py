1e847d976cb3b1d7cd6cca3399cc68e681c2bd2e
Diviyan Kalainathan <dkalainathan@gmail.com>
2016-09-12 17:27:17 +0200

    [C/E] Fixes and added multiprocessing Signed-off-by: Diviyan Kalainathan <dkalainathan@gmail.com>

--- Cause-effect/split_data.py
+++ Cause-effect/split_data.py
@@ -14,36 +14,55 @@ import time
 num_lines = 5000
 #inputfolder = 'output/subj6/'
 listfiles = ['pairs_c_', 'predictions_c_', 'publicinfo_c_']
-
-
+heads=[['SampleID','A','B'],['SampleID','A type','B type'],['SampleID','Target']]
 def splitfiles_cluster(inputfolder, cluster_n):
+
     for filetype in listfiles:
+        
         with open(inputfolder + 'cluster_' + str(cluster_n) + '/' + filetype + str(cluster_n) + '.csv') as pairs:
-            pairs_reader = csv.reader(pairs, delimiter=';')
-            header = next(pairs_reader)
-            splitfilepath = ''
-            rows_towrite = []
-            n_file = -1
-            for row in pairs_reader:
-                if (pairs_reader.line_num - 1) % num_lines == 0:
+            datareader = csv.reader(pairs, delimiter=';', quotechar='|')
+            header = next(datareader)
+            #splitfilepath = ''
+            #rows_towrite = [[]]
+
+            sampleID=[]
+            idx=0
+            n_file = 0
+            rows_towrite = [[]]
+            splitfilepath = inputfolder + 'split_data/cluster_' + str(cluster_n) + '/' + filetype + str(
+                cluster_n) + '_p' + str(n_file) + '.csv'
+            with open(splitfilepath, 'wb') as splitfile:
+                datawriter = csv.writer(splitfile, delimiter=';', quotechar='|')
+                datawriter.writerow(header)
+
+            for row in datareader:
+
+                if (datareader.line_num ) % num_lines == 0:
                     # Dump lines in old file
                     if n_file >= 0:
                         with open(splitfilepath, 'a') as splitfile:
                             datawriter = csv.writer(splitfile, delimiter=';', quotechar='|', lineterminator='\n')
                             for line in rows_towrite:
-                                datawriter.writerow(line)
-                        sys.stdout.write('--Cluster ' + str(cluster_n) + ' -- file ' + str(n_file)+'\n')
+                                if line !='':
+                                    datawriter.writerow(line)
+                        sys.stdout.write('--Cluster ' + str(cluster_n) + ' -- file ' + str(n_file) + '\n')
                         sys.stdout.flush()
+                        break #Test ! REMOVE
                     # Switch files
                     n_file += 1
-                    rows_towrite = []
+                    rows_towrite = [[]]
                     splitfilepath = inputfolder + 'split_data/cluster_' + str(cluster_n) + '/' + filetype + str(
                         cluster_n) + '_p' + str(n_file) + '.csv'
                     with open(splitfilepath, 'wb') as splitfile:
                         datawriter = csv.writer(splitfile, delimiter=';', quotechar='|')
                         datawriter.writerow(header)
 
-                rows_towrite += row
+                if row: # sort out blank lines
+                    sampleID+= row[0] #Replace ID w/ number?
+                    row[0]=str(idx)
+                    rows_towrite.append(row)
+                    idx+=1
+
             # Dump lines for last file
             if n_file >= 0:
                 with open(splitfilepath, 'a') as splitfile:
@@ -51,6 +70,15 @@ def splitfiles_cluster(inputfolder, cluster_n):
                     for line in rows_towrite:
                         datawriter.writerow(line)
 
+                #Create index
+                splitfilepath = inputfolder + 'split_data/cluster_' + str(cluster_n) + '/index_c_'+str(cluster_n) + '.csv'
+                with open(splitfilepath, 'wb') as splitfile:
+                    datawriter = csv.writer(splitfile, delimiter=';', quotechar='|')
+                    datawriter.writerow(['Idx','SampleID'])
+                    for i in range(len(sampleID)):
+                        datawriter.writerow([str(i),sampleID[i]])
+    sys.stdout.write('--Cluster ' + str(cluster_n) + ' -- Job finished !\n')
+    sys.stdout.flush()
 
 class file_splitter(threading.Thread):
     def __init__(self, inputfolder, cluster_n):
@@ -69,7 +97,7 @@ def split(inputfolder):
         os.makedirs(inputfolder + 'split_data')
 
     jobs = []
-    while os.path.exists(inputfolder + 'cluster_' + str(cluster_n)):
+    while os.path.exists(inputfolder + 'cluster_' + str(cluster_n)) and cluster_n<1:
         if not os.path.exists(inputfolder + 'split_data/cluster_' +str(cluster_n)):
             os.makedirs(inputfolder + 'split_data/cluster_' + str(cluster_n))
 
