273c49059026d36f8db7e480ab4eb0dd70da39a9
Diviyan Kalainathan <dkalainathan@gmail.com>
2016-09-29 18:13:34 +0200

    [C/E] Added link deconvolution Signed-off-by: Diviyan Kalainathan <dkalainathan@gmail.com>

--- Cause-effect/graph_construction.py
+++ Cause-effect/graph_construction.py
@@ -8,156 +8,260 @@ import csv
 import cPickle as pkl
 import numpy
 import scipy.stats as stats
+import sys
 
-inputfolder='output/obj8/pca_var/cluster_5/'
-causal_results=inputfolder+'cluster_5/...' #csv with 3 cols, Avar, Bvar & target
+inputfolder = 'output/obj8/pca_var/cluster_5/'
+causal_results = inputfolder + 'results_lp_CSP+Public_thres0.12.csv'  # csv with 3 cols, Avar, Bvar & target
 if 'obj8' in inputfolder:
-    obj=True
+    obj = True
 else:
-    obj=False
+    obj = False
 
-dependancy_matrix_type=1
+flags = False  # Taking account of flags
+
+skeleton_construction_method = int(sys.argv[1])
 """ Type of skeleton construction
+#0 : Skip and load computed data
 #1 : Absolute value of Pearson's correlation
 #2 : Regular value of Pearson's correlation
 #3 : Causation coefficient
 """
 
-mode = 1
+if sys.argv[1][0] == '0':  # Choose which data to load w/ arg of type "01"
+    load_skeleton = True
+else:
+    load_skeleton = False
+
+deconvolution_method = int(sys.argv[2])
+"""Method used for the deconvolution
+#1: Deconvolution according to Soheil Feizi
+#2: Recursive method according to Michele Sebag
 """
-#1 : Constructing graph w/ Michele's method:
-#2 : Isabelle's method/ "Classic" (Pearson's correlation + Deconvolution + orientation)
-"""
-if mode==1:
 
-    list_var = []  # create blank list for name of vars
-    causality_links = []  # List of links between vars
+print('Loading data')
+ordered_var_names = pkl.load(open('input/header.p'))
+if not flags:  # remove flag vars
+    ordered_var_names = [x for x in ordered_var_names if 'flag' not in x]
 
-    # Import data, construction of data structure
-    with open(causal_results,'rb') as inputfile:
-        reader=csv.reader(inputfile, delimiter=';')
-        header=next(reader)
+if obj == True:
+    num_axis = 8
+else:
+    num_axis = 5
 
-        for row in reader:
+for axis in range(num_axis):
+    ordered_var_names.append('pca_axis_' + str(axis + 1))
+    if flags:
+        ordered_var_names.append('pca_axis_' + str(axis + 1) + '_flag')
 
-            if (row[0]) not in list_var:
-                list_var.append(row[0])
-                causality_links.append([[],[]]) #0 for parents, 1 for children
+link_mat = numpy.ones((len(ordered_var_names), len(ordered_var_names)))  # Matrix of links, fully connected
+# 1 is linked and 0 unlinked,
 
-            if (row[1]) not in list_var:
-                list_var.append(row[1])
-                causality_links.append([[],[]]) #0 for parents, 1 for children
+print('Done.')
 
-            if float(row[3])>0:
-                causality_links[list_var.index(row[0])][1].append(list_var.index(row[1]))
-                causality_links[list_var.index(row[1])][0].append(list_var.index(row[0]))
+#### Pearson's correlation to remove links ####
+print('Creating link skeleton')
 
+if load_skeleton:
+    print('Skipping construction & loading values')
+    with open(inputfolder + 'link_mat_pval_' + str(skeleton_construction_method) + '.p', 'rb') as link_mat_file:
+        link_mat = pkl.load(link_mat_file)
+
+elif skeleton_construction_method < 3:
+    with open(inputfolder + 'pairs_c_5.csv', 'rb') as pairs_file:
+        datareader = csv.reader(pairs_file, delimiter=';')
+        header = next(datareader)
+        threshold = 0.1
+        var_1 = 0
+        var_2 = 0
+        # Idea: go through the vars and unlink the skipped (not in the pairs file) pairs of vars.
+        for row in datareader:
+            if row == []:  # Skipping blank lines
+                continue
+
+            pair = row[0].split('-')
+
+            if not flags and ('flag' in pair[0] or 'flag' in pair[1]):
+                continue  # Skipping values w/ flags
+
+            # Finding the pair var_1 var_2 corresponding to the line
+            # and un-linking skipped values
+            while pair[0] != ordered_var_names[var_1]:
+                if var_2 != len(ordered_var_names):
+                    link_mat[var_1, var_2 + 1:] = 0
+                var_1 += 1
+                var_2 = 0
+
+            skipped_value = False  # Mustn't erase checked values
+            while pair[1] != ordered_var_names[var_2]:
+                if skipped_value:
+                    link_mat[var_1, var_2] = 0
+                var_2 += 1
+                skipped_value = True
+
+            # Parsing values of table & removing artifacts
+            var_1_value = [float(x) for x in row[1].split(' ') if x is not '']
+            var_2_value = [float(x) for x in row[2].split(' ') if x is not '']
+
+            if len(var_1_value) != len(var_2_value):
+                raise ValueError
+
+            if abs(stats.pearsonr(var_1_value, var_2_value)[0]) > threshold:
+                if skeleton_construction_method == 1:
+                    link_mat[var_1, var_2] = abs(stats.pearsonr(var_1_value, var_2_value)[0])
+                elif skeleton_construction_method == 2:
+                    link_mat[var_1, var_2] = (stats.pearsonr(var_1_value, var_2_value)[0])
             else:
-                causality_links[list_var.index(row[0])][0].append(list_var.index(row[1]))
-                causality_links[list_var.index(row[1])][1].append(list_var.index(row[0]))
+                link_mat[var_1, var_2] = 0
 
-    with open(causal_results+'causality.pkl', 'wb') as handle:
-        pkl.dump(causality_links,handle)
+    # Symmetrize matrix
+    for col in range(0, (len(ordered_var_names) - 1)):
+        for line in range(col + 1, (len(ordered_var_names))):
+            link_mat[line, col] = link_mat[col, line]
 
-    with open(causal_results+'list_vars.pkl', 'wb') as handle:
-        pkl.dump(list_var,handle)
+    # Diagonal elts
+    for diag in range(0, (len(ordered_var_names))):
+        link_mat[diag, diag] = 0
 
+#### Causality score to remove links ####
 
-    #Creating all possible combinations:
+elif skeleton_construction_method == 3:
 
-    causality_possibilites=[[[],[]] for i in causality_links]
-    #ToDO ASK MICHELE FOR THE DATA STRUCTURE . INFINITE POSSIBILITIES CAN BE GENERATED
-     #1. Generate all lists
-     #2. Generate up to n parents & n children
+    with open(causal_results, 'rb') as pairs_file:
+        datareader = csv.reader(pairs_file, delimiter=';')
+        header = next(datareader)
+        threshold = 0.12
+        var_1 = 0
+        var_2 = 0
+        # Idea: go through the vars and unlink the skipped (not in the pairs file) pairs of vars.
+        for row in datareader:
 
-elif mode==2:
-    ordered_var_names=pkl.load(open('input/header.p'))
-    if obj==True:
-        num_axis=8
-    else:
-        num_axis=5
+            if not flags and ('flag' in row[0] or 'flag' in row[1]):
+                continue  # Skipping values w/ flags
 
-    for axis in range(num_axis):
-        ordered_var_names.append('pca_axis_'+str(axis+1))
-        ordered_var_names.append('pca_axis_' + str(axis + 1)+'_flag')
+            # Finding the pair var_1 var_2 corresponding to the line
+            # and un-linking skipped values
+            while row[0] != ordered_var_names[var_1]:
+                if var_2 != len(ordered_var_names):
+                    link_mat[var_1, var_2 + 1:] = 0
+                var_1 += 1
+                var_2 = 0
 
-    link_mat=numpy.ones((len(ordered_var_names),len(ordered_var_names))) #Matrix of links, fully connected
-    #1 is linked and 0 unlinked,
+            skipped_value = False  # Mustn't erase checked values
+            while row[1] != ordered_var_names[var_2]:
+                if skipped_value:
+                    link_mat[var_1, var_2] = 0
+                var_2 += 1
+                skipped_value = True
 
+            if float(row[2]) > threshold:
+                link_mat[var_1, var_2] = float(row[2])
 
-    #### Pearson's correlation to remove links ####
+    # Anti-symmetrize matrix
+    for col in range(0, (len(ordered_var_names) - 1)):
+        for line in range(col + 1, (len(ordered_var_names))):
+            link_mat[line, col] = -link_mat[col, line]
 
-    if dependancy_matrix_type<3:
-        with open(inputfolder+'pairs_c_5.csv','rb') as pairs_file:
-            datareader=csv.reader(pairs_file, delimiter=',')
-            header=next(datareader)
-            threshold=0.1
-            var_1=0
-            var_2=0
-            #Idea: go through the vars and unlink the skipped (not in the pairs file) pairs of vars.
-            for row in datareader:
-                pair= row[0].split('-')
+    # Diagonal elts
+    for diag in range(0, (len(ordered_var_names))):
+        link_mat[diag, diag] = 0
 
-                #Finding the pair var_1 var_2 corresponding to the line
-                # and un-linking skipped values
-                while pair[0]!=ordered_var_names[var_1]:
-                    if var_2!=len(ordered_var_names):
-                        link_mat[var_1,var_2+1:]=0
-                    var_1+=1
-                    var_2 = 0
+else:
+    raise ValueError
+if skeleton_construction_method != 0:
+    with open(inputfolder + 'link_mat_pval_' + str(skeleton_construction_method) + '.p', 'wb') as link_mat_file:
+        pkl.dump(link_mat, link_mat_file)
 
-                skipped_value=False #Mustn't erase checked values
-                while pair[1] != ordered_var_names[var_2]:
-                    if skipped_value:
-                        link_mat[var_1, var_2] = 0
-                    var_2 += 1
-                    skipped_value=True
+print('Done.')
 
-                #Parsing values of table & removing artifacts
-                var_1_value = [x for x in row[1].split(' ') if x is not '']
-                var_2_value = [x for x in row[2].split(' ') if x is not '']
+#### Loading causation data ####
+# Go through all nodes and remove redundant links
 
-                if len(var_1_value)!=len(var_2_value):
-                    raise ValueError
+list_var = []  # create blank list for name of vars
+causality_links = []  # List of links between vars
+# Init list var and causation links:
+for name_var in ordered_var_names:
+    list_var.append(name_var)
+    causality_links.append([[], []])
 
-                if  abs(stats.pearsonr(var_1_value,var_2_value)[0])>threshold:
-                    if dependancy_matrix_type==1:
-                        link_mat[var_1, var_2]= abs(stats.pearsonr(var_1_value,var_2_value)[0])
-                    elif dependancy_matrix_type==2:
-                        link_mat[var_1, var_2] = (stats.pearsonr(var_1_value, var_2_value)[0])
-                else: link_mat[var_1, var_2] = 0
-        #ToDo Symmetrize matrix
-    elif dependancy_matrix_type==3:
+# Import data, construction of data structure
+with open(causal_results, 'rb') as inputfile:
+    reader = csv.reader(inputfile, delimiter=';')
+    header = next(reader)
 
-        with open(inputfolder + 'results_lp.csv', 'rb') as pairs_file:
-            datareader = csv.reader(pairs_file, delimiter=';')
-            header = next(datareader)
-            threshold = 0.12
-            var_1 = 0
-            var_2 = 0
-            # Idea: go through the vars and unlink the skipped (not in the pairs file) pairs of vars.
-            for row in datareader:
+    for row in reader:
 
-                # Finding the pair var_1 var_2 corresponding to the line
-                # and un-linking skipped values
-                while row[0] != ordered_var_names[var_1]:
-                    if var_2 != len(ordered_var_names):
-                        link_mat[var_1, var_2 + 1:] = 0
-                    var_1 += 1
-                    var_2 = 0
+        if (row[0]) not in list_var:
+            list_var.append(row[0])
+            causality_links.append([[], []])  # 0 for parents, 1 for children
 
-                skipped_value = False  # Mustn't erase checked values
-                while row[1] != ordered_var_names[var_2]:
-                    if skipped_value:
-                        link_mat[var_1, var_2] = 0
-                    var_2 += 1
-                    skipped_value = True
+        if (row[1]) not in list_var:
+            list_var.append(row[1])
+            causality_links.append([[], []])  # 0 for parents, 1 for children
 
-                if float(row[2])>threshold:
-                    link_mat[var_1, var_2] = float[row[2]]
+        if float(row[2]) > 0:
+            causality_links[list_var.index(row[0])][1].append(list_var.index(row[1]))
+            causality_links[list_var.index(row[1])][0].append(list_var.index(row[0]))
 
-        #ToDo Anti-Symmetrize matrix
+        else:
+            causality_links[list_var.index(row[0])][0].append(list_var.index(row[1]))
+            causality_links[list_var.index(row[1])][1].append(list_var.index(row[0]))
 
-    pkl.dump(link_mat,open(inputfolder+'link_mat_pval.p'))
+with open(causal_results + 'causality.pkl', 'wb') as handle:
+    pkl.dump(causality_links, handle)
 
-    #### Apply deconvolution ####
+with open(causal_results + 'list_vars.pkl', 'wb') as handle:
+    pkl.dump(list_var, handle)
+
+#### Apply deconvolution ####
+print('Deconvolution')
+if deconvolution_method == 1:
+    """This is a python implementation/translation of network deconvolution
+
+ AUTHORS:
+    Algorithm was programmed by Soheil Feizi.
+    Paper authors are S. Feizi, D. Marbach,  M. Medard and M. Kellis
+Python implementation: Gideon Rosenthal
+
+REFERENCES:
+   For more details, see the following paper:
+    Network Deconvolution as a General Method to Distinguish
+    Direct Dependencies over Networks
+    By: Soheil Feizi, Daniel Marbach,  Muriel Medard and Manolis Kellis
+    Nature Biotechnology"""  # Credits, Ref
+
+    Gdir = numpy.dot(link_mat, numpy.linalg.inv(numpy.identity(len(ordered_var_names)) + link_mat))
+
+elif deconvolution_method == 2:
+
+    # Creating all possible combinations:
+    Gdir = link_mat
+    causality_possibilites = [[[], []] for i in causality_links]
+    # ToDO
+    # 1. Generate all lists
+    # 2. Generate up to n parents & n children
+else:
+    raise ValueError
+print('Done.')
+
+#### Output values ####
+print('Writing output files')
+with open(inputfolder + 'deconv_links' + str(skeleton_construction_method) + str(deconvolution_method) + '.csv',
+          'wb') as outputfile:
+    writer = csv.writer(outputfile, delimiter=';', lineterminator='\n')
+    writer.writerow(['Source', 'Target', 'Value'])
+    for var_1 in range(len(ordered_var_names) - 1):
+        for var_2 in range(var_1 + 1, len(ordered_var_names)):
+            if abs(Gdir[var_1, var_2]) > 0.001: #ignore value if it's near 0
+                # Find the causal direction
+                if list_var.index(ordered_var_names[var_2]) in \
+                        causality_links[list_var.index(ordered_var_names[var_1])][1]:
+                    # var_2 is the child
+                    writer.writerow([ordered_var_names[var_1], ordered_var_names[var_2], abs(Gdir[var_1, var_2])])
+                elif list_var.index(ordered_var_names[var_2]) in \
+                        causality_links[list_var.index(ordered_var_names[var_1])][
+                            0]:
+                    # Var_2 is the parent
+                    writer.writerow([ordered_var_names[var_2], ordered_var_names[var_1], abs(Gdir[var_1, var_2])])
+
+print('Done.')
+print('End of program.')
