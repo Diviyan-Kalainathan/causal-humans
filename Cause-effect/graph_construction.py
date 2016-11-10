19bf3efe4c1c514fcb25e2dc98a16e66bb6e5a6d
Diviyan Kalainathan <dkalainathan@gmail.com>
2016-11-10 17:44:08 +0100

    [C/E] Graph construction : added other ROC curves & scores
    Signed-off-by: Diviyan Kalainathan <dkalainathan@gmail.com>

--- Cause-effect/graph_construction.py
+++ Cause-effect/graph_construction.py
@@ -23,11 +23,12 @@ test_set_name = 'net1_expression_data_InSilico'  # 'net3_expression_data_Ecoli_t
 
 filenameData = test_set_name + ".tsv"
 
-outputPath = "output/Dream5/"
+outputPath = "output/Dream5.2/"
 outputCausalityPairs = test_set_name + "_pairs.csv"
 outputPublicInfo = test_set_name + "_publicinfo.csv"
 outputforCausation = test_set_name + "_fcausation.csv"
-outputResults = test_set_name + "_results.csv"
+outputResults = test_set_name + "_results.tsv"
+outputResultsContrib = test_set_name + "_results_contrib.tsv"
 outputTarget = path + test_set_name + "_target.tsv"
 
 df_input = pd.read_csv(path + filenameData, sep='\t', encoding="latin-1")
@@ -36,15 +37,16 @@ if len(sys.argv) < 2:
     raise ValueError
 nb_proc = int(sys.argv[1])
 independancy_criterion = 0
-deconvolution_method = 3
-predict_method = 1
+deconvolution_method = 1
+predict_method = 2
 thresholdDepLink = 0.01
 # epsilon=0.5
 beta = 0.5
 alpha = 0.1
 
 """
-#1 : "Pearson's correlation",
+#0 : "Pearson's correlation"
+#1 : "Abs Pearson's correlation",
 #2 : "Pval-Pearson",
 #3 : "Chi2 test",
 #4 : "Mutual information",
@@ -187,7 +189,7 @@ for i in range(0, Gdir.shape[0]):
 
     for j in range(i + 1, Gdir.shape[1]):
 
-        if (Gdir[i, j] > thresholdDepLink):
+        if (abs(Gdir[i, j]) > thresholdDepLink):
             a = df_input.iloc[:, i].values
             b = df_input.iloc[:, j].values
 
@@ -206,7 +208,8 @@ for i in range(0, Gdir.shape[0]):
 
             df_publicinfo = pd.concat([df_publicinfo, newLignPublicinfo], ignore_index=True)
 
-print(df_output)
+print('Number of pairs : '),
+print(len(df_output.index))
 df_output.to_csv(outputPath + outputCausalityPairs, index=False, encoding='utf-8', sep=",")
 df_publicinfo.to_csv(outputPath + outputPublicInfo, index=False, encoding='utf-8', sep=",")
 print('...Done.')
@@ -239,7 +242,11 @@ df_results.columns = ['Source', 'Target', 'Score']
 df_results = df_results.sort_values(by='Score', ascending=False)
 df_results.to_csv(outputPath + outputResults, sep='\t', index=False)
 
+
+################################################# Results ############################################################
+
 #### Compare results to target values ####
+
 df_target = pd.read_csv(outputTarget, sep='\t', encoding="latin-1")
 df_target.columns = ['Source', 'Target', 'Score']
 
@@ -288,7 +295,7 @@ plt.show()
 plt.savefig(outputPath + test_set_name + 'results_directed.pdf')
 plt.clf()
 
-#### Results on structure ####
+#### Results on structure w/ causation coefficient ####
 
 P = float(len(df_target.index))
 N = float(len(var_names) * (len(var_names) - 1)) / 2 - P
@@ -335,5 +342,127 @@ ax2.set_title('Precision recall curve on graph structure')
 ax2.legend(loc='best')
 plt.show()
 plt.savefig(outputPath + test_set_name + 'results_not_directed.pdf')
+plt.clf()
+
+#### Results on structure after deconvolution ####
+
+P = float(len(df_target.index))
+N = float(len(var_names) * (len(var_names) - 1)) / 2 - P
+
+TP = 0.0
+FP = 0.0
+tpr = []  # = recall
+fpr = []
+ppv = []
+
+for i in range(0, Gdir.shape[0]):
+    for j in range(i + 1, Gdir.shape[1]):
+
+        if (abs(Gdir[i, j]) > 0.001):
+            if (((df_target['Source'] == var_names[i]) & (df_target['Target'] == var_names[j]))
+                    | (df_target['Source'] == var_names[j]) & (
+                            df_target['Target'] == var_names[i])).any():  # Scores  are only 1 :
+                    # Need to modify if scores are different than 1.
+                    TP += 1
+            else:
+                FP += 1
+
+            tpr.append(TP / P)  # TPR=recall
+            fpr.append(FP / N)  # FPR
+            ppv.append(TP / (TP + FP))
+
+auc_pr_score = auc(tpr, ppv, reorder=True)
+auc_roc_score = auc(fpr, tpr, reorder=True)
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+
+pl1 = ax1.plot(fpr, tpr, label=' (area: {0:3f})'.format(auc_roc_score), color='r')
+pl2 = ax2.plot(tpr, ppv, label=' (area: {0:3f})'.format(auc_pr_score), color='r')
+
+ax1.plot([0, 1], [0, 1], linestyle='--', color='k',
+label='Luck')
+
+ax1.set_xlabel('False Positive Rate')
+ax1.set_ylabel('True Positive Rate')
+ax1.set_title('ROC Curve on graph structure')
+ax1.legend(loc="lower right")
+ax2.set_xlabel('Recall')
+ax2.set_ylabel('Precision')
+ax2.set_title('Precision recall curve on graph structure')
+ax2.legend(loc='best')
+plt.show()
+plt.savefig(outputPath + test_set_name + 'results_not_directed.pdf')
+plt.clf()
+
+#### Results on links with both contributions ####
+
+idx1=0
+idx2=1
+contrib_results=[]
+for idx, row in df_causation_results.iterrows():
+    var=row['SampleID'].split('-')
+    while ((var[0]!=var_names[idx1] | var[1]!=var_names[idx2])&(var[1]!=var_names[idx1] | var[0]!=var_names[idx2])):
+        idx2+=1
+        if idx2==len(var_names):
+            idx1+=1
+            idx2=idx1+1
+            if idx1==len(var_names):
+                print('Vars Not ordered')
+                raise ValueError
+    score= abs(Gdir[idx1,idx2])*row['Score']
+    if score>0:
+        contrib_results.append([var[0],var[1],score])
+    else:
+        contrib_results.append([var[1],var[0],score])
+
+df_results_contrib=pd.DataFrame(contrib_results,columns=['Source','Target','Score'])
+df_results_contrib=df_results_contrib.sort_values(by='Score', ascending=False)
+df_results_contrib.to_csv(outputPath + outputResultsContrib, sep='\t', index=False)
+
+P = float(len(df_target.index))
+N = float(len(var_names) * (len(var_names) - 1)) - P
+
+TP = 0.0
+FP = 0.0
+tpr = []  # = recall
+fpr = []
+ppv = []
+
+for idx, row in df_results_contrib.iterrows():
+    if ((df_target['Source'] == row['Source']) & (df_target['Target'] == row['Target'])).any():  # Scores  are only 1 :
+        # Need to modify if scores are different than 1.
+        TP += 1
+    else:
+        FP += 1
+
+    tpr.append(TP / P)  # TPR=recall
+    fpr.append(FP / N)  # FPR
+    ppv.append(TP / (TP + FP))
+
+auc_pr_score = auc(tpr, ppv, reorder=True)
+auc_roc_score = auc(fpr, tpr, reorder=True)
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111)
+
+pl1 = ax1.plot(fpr, tpr, label=' (area: {0:3f})'.format(auc_roc_score), color='r')
+pl2 = ax2.plot(tpr, ppv, label=' (area: {0:3f})'.format(auc_pr_score), color='r')
+
+ax1.plot([0, 1], [0, 1], linestyle='--', color='k',
+         label='Luck')
+
+ax1.set_xlabel('False Positive Rate')
+ax1.set_ylabel('True Positive Rate')
+ax1.set_title('ROC Curve on directed graph w/ both contributions')
+ax1.legend(loc="lower right")
+ax2.set_xlabel('Recall')
+ax2.set_ylabel('Precision')
+ax2.set_title('Precision recall curve on directed graph w/ both contributions')
+ax2.legend(loc='best')
+plt.show()
+plt.savefig(outputPath + test_set_name + 'results_directed_contrib.pdf')
 plt.clf()
 print('End of program.')
