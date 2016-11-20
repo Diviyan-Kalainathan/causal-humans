c7fc454266e4e8a77c48bb508a1a2e56d0c54d12
Diviyan Kalainathan <dkalainathan@gmail.com>
2016-11-20 13:00:01 +0100

    [C/E] Added and began to adapt protoML
    Signed-off-by: Diviyan Kalainathan <dkalainathan@gmail.com>

--- Cause-effect/util/file_to_kagf.py
+++ Cause-effect/util/file_to_kagf.py
@@ -21,27 +21,29 @@ def file_to_kag(pthtofile):
     type_info=[]
     i=1
     #with open(pthtofile+'final_Pairs.csv','wb') as output:
-        #writer=csv.writer(output)
-        #writer.writerow(['SampleID','A','B'])
+    #    writer=csv.writer(output)
+    #    writer.writerow(['SampleID','A','B'])
 
     while os.path.exists(pthtofile+str(i)+'.txt'):
         print(pthtofile+str(i)+'.txt')
         #df=pd.read_csv(pthtofile+str(i)+'.txt',sep=' ')
         #A,B=format_col(df)
-        SampleID= os.path.basename(pthtofile)+'D'+str(i)
+        SampleID= os.path.basename(pthtofile)+'E'+str(i)
         SplID.append(SampleID)
         type_info.append('Numerical')
         #writer.writerow([SampleID,A,B])
         i+=1
 
-    print(SplID)
-    print(type_info)
-    info=pd.DataFrame()
-    info['SampleID']=SplID
-    info['Type-A']=type_info
-    info['Type-B']=type_info
+    print(len(SplID))
+    print(len(type_info))
+    info=pd.DataFrame([SplID,type_info,type_info])
+    info=info.transpose()
+    info.columns=['SampleID','Type-A','Type-B']
+    #info['SampleID']=pd.Series(SplID,index=info.index)
+    #info['Type-A']=pd.Series(type_info,index=info.index)
+    #info['Type-B']=pd.Series(type_info,index=info.index)
     info.to_csv(pthtofile+'final_publicinfo.csv',index=False)
 if __name__ == '__main__':
 
-    inputfile_type='/home/kaydhi/Documents/CausalPairwiseC/DLP_Generator/pair'
+    inputfile_type='/users/ao/diviyan/NoSave/MPI_Generator/TestANN/testsimv2/pair'
     file_to_kag(inputfile_type)
