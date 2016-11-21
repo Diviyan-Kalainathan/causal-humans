46baa21e07829c2b18750196aa03580c788ee60e
Diviyan Kalainathan <dkalainathan@gmail.com>
2016-11-21 22:09:48 +0100

    [Benchmark] Fixed filetoKag
    Signed-off-by: Diviyan Kalainathan <dkalainathan@gmail.com>

--- Cause-effect/util/file_to_kagf.py
+++ Cause-effect/util/file_to_kagf.py
@@ -9,30 +9,30 @@ import csv
 
 def format_col(df):
     df.columns=['A','B']
-    A=[]
-    B=[]
+    A=""
+    B=""
     for idx,row in df.iterrows():
-        A.append(' '+str(float(row['A'])))
-        B.append(' ' + str(float(row['B'])))
+        A+=' '+str(float(row['A']))
+        B+=' ' + str(float(row['B']))
     return A,B
 
 def file_to_kag(pthtofile):
     SplID=[]
     type_info=[]
     i=1
-    #with open(pthtofile+'final_Pairs.csv','wb') as output:
-    #    writer=csv.writer(output)
-    #    writer.writerow(['SampleID','A','B'])
+    with open(pthtofile+'Dfinal_Pairs.csv','wb') as output:
+        writer=csv.writer(output)
+        writer.writerow(['SampleID','A','B'])
 
-    while os.path.exists(pthtofile+str(i)+'.txt'):
-        print(pthtofile+str(i)+'.txt')
-        #df=pd.read_csv(pthtofile+str(i)+'.txt',sep=' ')
-        #A,B=format_col(df)
-        SampleID= os.path.basename(pthtofile)+'E'+str(i)
-        SplID.append(SampleID)
-        type_info.append('Numerical')
-        #writer.writerow([SampleID,A,B])
-        i+=1
+        while os.path.exists(pthtofile+str(i)+'.txt'):
+            print(pthtofile+str(i)+'.txt')
+            df=pd.read_csv(pthtofile+str(i)+'.txt',sep='    |   |  | ')
+            A,B=format_col(df)
+            SampleID= os.path.basename(pthtofile)+'D'+str(i)
+            SplID.append(SampleID)
+            type_info.append('Numerical')
+            writer.writerow([SampleID,A,B])
+            i+=1
 
     print(len(SplID))
     print(len(type_info))
@@ -45,5 +45,5 @@ def file_to_kag(pthtofile):
     info.to_csv(pthtofile+'final_publicinfo.csv',index=False)
 if __name__ == '__main__':
 
-    inputfile_type='/users/ao/diviyan/NoSave/MPI_Generator/TestANN/testsimv2/pair'
+    inputfile_type='/home/diviyan/Documents/CausalPairwiseC/DLP_Generator/txtfiles/pair'
     file_to_kag(inputfile_type)
